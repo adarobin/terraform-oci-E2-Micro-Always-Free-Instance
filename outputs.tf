f3499e20b3e381bcd1dbf923aca796a1dd4382d3
Adam Robinson <adam.e.robinson@icloud.com>
2021-08-04 20:02:19 -0400

    add output for instance id

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "public_ip" {
   description = "The public IP address assigned to the instance."
   value       = oci_core_instance.micro.public_ip
 }
+
+output "instance_id" {
+  description = "The OCID of the instance that was created"
+  value       = oci_core_instance.micro.id
+}
